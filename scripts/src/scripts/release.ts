33287006a48e1c0d1e767613e2d02ce6d1610adc
王首人 <wangshouren@wangshourendeMacBook-Air.local>
2024-05-06 09:41:01 +0800

    chore: 修改 release 脚本

--- scripts/src/scripts/release.ts
+++ scripts/src/scripts/release.ts
@@ -1,10 +1,16 @@
-import { findWorkspaceProjects } from "@/utils";
+import {
+  IProject,
+  PKG_ROOT,
+  VERSION_FILE,
+  findWorkspaceProjects,
+} from "@/utils";
 import { Script } from "@/models/script";
 import { $ } from "execa";
 import { simpleGit } from "simple-git";
 import { readdir } from "fs/promises";
 import { join, resolve } from "path";
 import { build } from "tsup";
+import { readFile } from "fs-extra";
 
 const git = simpleGit();
 
@@ -23,10 +29,10 @@ export default class extends Script<{}> {
       throw new Error("请提交后执行");
     }
 
-    await buildModules();
-
     // 更新版本号
-    const targetVersion = "0.0.1";
+    const targetVersion = await readFile(VERSION_FILE, "utf-8");
+
+    console.log(`upgrade to ${targetVersion}`);
 
     // 生成 changelog
     await $`pnpm conventional-changelog -p angular -i ./CHANGELOG.md -s`;
@@ -34,52 +40,50 @@ export default class extends Script<{}> {
     // 生成 git commit
     await git.add(".");
     await git.commit(`chore: update version to ${targetVersion}`);
+    await git.push("origin");
 
     // 生成 git tag
     await git.addTag(`release/v${targetVersion}`);
-
-    // 推送 git tag
-    // await git.pushTags("origin");
+    await git.pushTags("origin");
 
     // 发布到 npm
-    // const pkgs = await findWorkspaceProjects();
-    // await Promise.all(
-    //   pkgs.map((x) =>
-    //     x.writeProjectManifest({
-    //       ...x.manifest,
-    //       version: targetVersion,
-    //       module: "dist/index.js",
-    //       types: "dist/index.d.ts",
-    //       files: ["dist"],
-    //       main: undefined,
-    //       devDependencies: undefined,
-    //       scripts: undefined,
-    //     })
-    //   )
-    // );
-    // await $`pnpm publish -r --access public --no-git-checks`;
+    await buildPackages();
+    const pkgs = await findWorkspaceProjects({
+      excludeRoot: true,
+      patterns: ["packages/*"],
+    });
+    await Promise.all(
+      pkgs.map((x) =>
+        x.writeProjectManifest({
+          ...x.manifest,
+          version: targetVersion,
+          module: "dist/index.js",
+          types: "dist/index.d.ts",
+          files: ["dist"],
+          main: undefined,
+          devDependencies: undefined,
+          scripts: undefined,
+        })
+      )
+    );
+    await $`pnpm publish -r --access public --no-git-checks`;
   }
 }
 
-async function buildModules() {
-  const pkgDirs = (await readdir("packages")).map((x) =>
-    resolve("packages", x)
-  );
-  // await pkgDirs.reduce(
-  //   (task, pkgDir) => task.then(() => buildPkgDir(pkgDir)),
-  //   Promise.resolve()
-  // );
+async function buildPackages() {
+  const projects = await findWorkspaceProjects({
+    excludeRoot: true,
+    patterns: ["packages/*"],
+  });
+  await Promise.all(projects.map((x) => buildPkgDir(x)));
 
-  await Promise.all(pkgDirs.map((pkgDir) => buildPkgDir(pkgDir)));
-
-  async function buildPkgDir(pkgDir: string) {
-    const packageJson = require(join(pkgDir, "package.json"));
-    console.log(`Building ${packageJson.name}`);
+  async function buildPkgDir(project: IProject) {
+    console.log(`Building ${project.manifest.name}`);
 
     await build({
-      entry: [join(pkgDir, "src", "index.ts")],
+      entry: [join(project.dir, "src", "index.ts")],
       dts: true,
-      outDir: join(pkgDir, "dist"),
+      outDir: join(project.dir, "dist"),
       silent: true,
       clean: true,
     });
--- scripts/src/utils/path.ts
+++ scripts/src/utils/path.ts
@@ -1,18 +1,25 @@
-import path, { relative } from 'path';
+import path, { relative } from "path";
 
-export const PKG_ROOT = path.join(__dirname, '..', '..', '..');
-export const WEBSITE_ROOT = path.join(PKG_ROOT, 'packages', 'website');
-export const WEBSITE_TEMPLATE = path.join(WEBSITE_ROOT, '.dumi', 'modules', 'playground', 'template');
-export const WEBSITE_PUBLIC = path.join(WEBSITE_ROOT, 'public');
-export const WEBSITE_OUT = path.join(WEBSITE_ROOT, 'out');
-export const SDK_ROOT = path.join(PKG_ROOT, 'packages', 'sdk');
-export const SDK_DIST = path.join(SDK_ROOT, 'dist');
-export const SERVER_ROOT = path.join(PKG_ROOT, 'packages', 'server');
-export const SERVER_DOCS_ROOT = path.join(SERVER_ROOT, 'docs');
-export const CUSTOM_ROOT = path.join(PKG_ROOT, 'packages', 'custom');
-export const SCRIPTS_ROOT = path.join(PKG_ROOT, 'scripts');
-export const SCRIPTS_PLACED_ROOT = path.join(SCRIPTS_ROOT, 'src', 'scripts');
-export const DG29_ROOT = path.join(PKG_ROOT, 'packages', 'dg29');
+export const PKG_ROOT = path.join(__dirname, "..", "..", "..");
+export const VERSION_FILE = path.join(PKG_ROOT, ".version");
+export const WEBSITE_ROOT = path.join(PKG_ROOT, "packages", "website");
+export const WEBSITE_TEMPLATE = path.join(
+  WEBSITE_ROOT,
+  ".dumi",
+  "modules",
+  "playground",
+  "template"
+);
+export const WEBSITE_PUBLIC = path.join(WEBSITE_ROOT, "public");
+export const WEBSITE_OUT = path.join(WEBSITE_ROOT, "out");
+export const SDK_ROOT = path.join(PKG_ROOT, "packages", "sdk");
+export const SDK_DIST = path.join(SDK_ROOT, "dist");
+export const SERVER_ROOT = path.join(PKG_ROOT, "packages", "server");
+export const SERVER_DOCS_ROOT = path.join(SERVER_ROOT, "docs");
+export const CUSTOM_ROOT = path.join(PKG_ROOT, "packages", "custom");
+export const SCRIPTS_ROOT = path.join(PKG_ROOT, "scripts");
+export const SCRIPTS_PLACED_ROOT = path.join(SCRIPTS_ROOT, "src", "scripts");
+export const DG29_ROOT = path.join(PKG_ROOT, "packages", "dg29");
 
 export function getRelativeFromPkgRoot(path: string) {
   return `./${relative(PKG_ROOT, path)}`;
@@ -29,7 +36,7 @@ export function getRelativeImportPath(from: string, to: string) {
 }
 
 export function formatRelativeImportPath(relative: string) {
-  const rel: string = relative.replace(/(\.d)?\.(c|m)?ts$/, '');
+  const rel: string = relative.replace(/(\.d)?\.(c|m)?ts$/, "");
 
-  return rel.startsWith('../') ? rel : rel.startsWith('./') ? rel : `./${rel}`;
+  return rel.startsWith("../") ? rel : rel.startsWith("./") ? rel : `./${rel}`;
 }
