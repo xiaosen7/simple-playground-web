1643177637d896ed7492a0f4319a867329b04b5f
王首人 <wangshouren@wangshourendeMacBook-Air.local>
2024-05-20 09:35:26 +0800

    Updates

--- assets/javascript-BmepoA8u.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-D-fh4qQe.js";import"./undomanager-AZPc4tPd.js";import"./iframe-DKKRd2zT.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.48.0(0037b13fb5d186fdf1e7df51a9416a2de2b8c670)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var n=t,c={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{n as conf,c as language};
